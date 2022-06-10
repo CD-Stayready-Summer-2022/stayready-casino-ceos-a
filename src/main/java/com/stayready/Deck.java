45ec61ea3b2eec5e39a0c7dc37be907ee8970079
AbdullahAl-rubaye <93665223+AbdullahAl-rubaye@users.noreply.github.com>
2022-06-10 14:19:10 -0400

    add hand

--- src/main/java/com/stayready/Deck.java
+++ src/main/java/com/stayready/Deck.java
@@ -1,6 +1,9 @@
 package com.stayready;
 
-import com.stayready.games.Card;
+import com.stayready.games.CardGame;
+import com.stayready.games.cards.card.Card;
+import com.stayready.games.cards.card.CardSuit;
+import com.stayready.games.cards.card.CardValue;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,29 +11,33 @@ import java.util.Collections;
 public class Deck {
     private ArrayList<Card> cards;
 
-    public Deck(){
+    public Deck() {
         cards = new ArrayList<>();
         buildDeck();
     }
 
-    private void buildDeck(){
-        for(CardSuit suit : CardSuit.values()){
-            for(CardValue value : CardValue.values()){
+    private void buildDeck() {
+        for (CardSuit suit : CardSuit.values()) {
+            for (CardValue value : CardValue.values()) {
                 Card card = new Card(value, suit);
                 cards.add(card);
             }
         }
     }
 
-    public Card deal(Integer numberOfCards){
+    public CardGame deal(Integer numberOfCards) {
+
         return null;
+       
+
+
     }
 
-    public Integer cardsRemaining(){
+    public Integer cardsRemaining() {
         return cards.size();
     }
 
-    public void shuffle(){
+    public void shuffle() {
         Collections.shuffle(cards);
     }
 
