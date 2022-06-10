c578a7772728000901cf4c257aa0b1cda556a564
Boyce007 <diboyce21@students.desu.edu>
2022-06-10 11:19:09 -0400

    Updated Card Class

---
+++ src/main/java/com/stayready/games/cards/card/Card.java
@@ -0,0 +1,24 @@
+package com.stayready.games.cards.card;
+
+public class Card {
+    private CardValue value;
+    private CardSuit suit;
+
+    public Card(CardValue value, CardSuit suit){
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public CardValue getValue() {
+        return value;
+    }
+
+    public CardSuit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Suit: %s, Value: %s", suit.name, value.name);
+    }
+}
