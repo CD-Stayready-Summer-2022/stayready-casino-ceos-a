25cd24878dee14a77468b6e47375e5e1d4b61fcd
AbdullahAl-rubaye <93665223+AbdullahAl-rubaye@users.noreply.github.com>
2022-06-12 22:16:53 -0400

    test

--- src/test/java/com/stayready/games/HandTest.java
+++ src/test/java/com/stayready/games/HandTest.java
@@ -1,6 +1,51 @@
 package com.stayready.games;
 
-public class HandTest {
+import com.stayready.stayready.games.cards.card.Card;
+import com.stayready.stayready.games.cards.card.CardSuit;
+import com.stayready.stayready.games.cards.card.CardValue;
+import com.stayready.stayready.games.cards.card.Hand;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-    
+public class HandTest {
+    @Test
+    @DisplayName("value of hand test")
+
+    public void constructerTest(){
+        Hand hand=new Hand();
+        Assertions.assertNotNull(hand);
+    }
+    @Test
+    @DisplayName("value of card in hand")
+    public void valueOfCardInHandTest(){
+        Hand hand=new Hand();
+        Card card=new Card(CardValue.TWO, CardSuit.SPADE);
+        hand.giveCardToHand(card);
+
+        Assertions.assertTrue(hand.valueOfCardInHand(CardValue.TWO));
+    }
+    @Test
+    @DisplayName("give card to hanf")
+    public void giveCardToHandTest(){
+        Hand hand=new Hand();
+        Card card=new Card(CardValue.TWO, CardSuit.SPADE);
+        hand.giveCardToHand(card);
+        Integer expected=1;
+        Integer actual=hand.getSize();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("get card from hand")
+    public void getCardFromHandTest(){
+        Hand hand=new Hand();
+        Card card=new Card(CardValue.TWO, CardSuit.SPADE);
+        hand.giveCardToHand(card);
+        hand.getCardFromHand(card);
+        Integer expected=0;
+        Integer actual=hand.getSize();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
